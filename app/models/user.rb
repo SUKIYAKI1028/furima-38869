32eaa7b596e7f79cc52e5604e452bf66da587829
SUKIYAKI1028 <sukiyakinosuke@gmail.com>
2023-01-29 13:42:23 +0900

    RSpec事前準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
