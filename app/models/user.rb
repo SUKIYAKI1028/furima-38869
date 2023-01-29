c75ae62eecffeb26135f4b2ea8b6478fe2c1b6ae
SUKIYAKI1028 <sukiyakinosuke@gmail.com>
2023-01-30 02:39:10 +0900

    テストコード記述
    factories/user.rb編集（Faker記述）
    gem(pry-rails)導入
    エラー発生時にメッセージが表示の記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates :encrypted_password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -10,10 +15,6 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :birthday, presence: true
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   has_many :comments
   has_many :items
   has_many :orders
