b02e5ea4034a6f95d568ff468ea1f61bef14f3d7
SUKIYAKI1028 <sukiyakinosuke@gmail.com>
2023-01-31 16:54:42 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+  belongs_to :user
 end
