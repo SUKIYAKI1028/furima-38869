be71f46d6218f404b80f3d9023e284a6b5a1bde6
SUKIYAKI1028 <sukiyakinosuke@gmail.com>
2023-02-07 16:44:05 +0900

    購入機能実装①

---
+++ app/models/item_order.rb
@@ -0,0 +1,16 @@
+class ItemOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :telephone_number, :order_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :city, :block, :user_id, :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :telephone_number, format: { with: /\A[0-9]{11}\z/ }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, telephone_number: telephone_number)
+  end
+end
