938704bd0bdf3d31162edfe6285bef5d41a32550
SUKIYAKI1028 <sukiyakinosuke@gmail.com>
2023-02-09 16:02:23 +0900

    一部追加修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,8 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if user_signed_in? && current_user.id == @item.user_id && @item.order.nil?
-    else
+    if @item.user_id != current_user.id  || @item.order.present? 
       redirect_to root_path
     end
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,13 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    if user_signed_in? && current_user.id != @item.user_id && @item.order.nil?
-      @item_order = ItemOrder.new
-    else
+    if @item.user_id == current_user.id  || 
+     @item.order.present?
       redirect_to root_path
     end
+    @order_address = ItemOrder.new
   end
-
+  
   def create
     @item_order = ItemOrder.new(order_params)
     if @item_order.valid?
